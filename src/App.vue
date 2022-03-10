74423e244f191cbcd24860bf083bafb7af891aa0
EWorld90 <hc980729@gmail.com>
2022-03-10 17:28:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import TestButton from './components/TestButton.vue'
+import Login from './components/Login.vue'
+</script>
+
+<template>
+  <!--
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <TestButton></TestButton>
+  -->
+  <Login></Login>
+</template>
+
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+html, body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+#app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<script setup>
+</script>
+
+<template>
+    <div class="login-panel">
+        <div class="test-class">test</div>
+        <div class="login-area"></div>
+    </div>
+</template>
+
+<style scoped>
+/* * {
+    margin: 0;
+    padding: 0;
+} */
+.login-panel {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-image: url(../assets/login-bg2.jpg);
+    background-size: 100%;
+    background-position: center top;
+    background-repeat: no-repeat;
+}
+
+.login-area {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 300px;
+    height: 300px;
+    margin: -190px 0 0 -175px;
+    background-color: rgba(255, 255, 255, 0.5);
+}
+.test-class {
+    overflow: hidden;
+}
+</style>
