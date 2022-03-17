56ddfeb5adfd92e0af34f260dc4821c222046206
EWorld90 <hc980729@gmail.com>
2022-03-17 17:19:39 +0800

    fix

--- src/components/BaseTable.vue
+++ src/components/BaseTable.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import { ElMessageBox, ElMessage } from 'element-plus'
 import axios from 'axios'
 
@@ -8,6 +8,7 @@ import {
     Plus
 } from '@element-plus/icons-vue'
 
+// 表格信息
 const tableData = ref([]);
 
 // 获取表格信息
@@ -28,11 +29,11 @@ const getTableData = () => {
 
 // 格式化表格信息
 const formatTableData = (tableDataTemp) => {
-    for(let i = 0; i < tableDataTemp.length; i++) {
+    for (let i = 0; i < tableDataTemp.length; i++) {
         // TODO 获取数据库中状态 id 与状态名称的关系
-        if(tableDataTemp[i].status === 1) {
+        if (tableDataTemp[i].status === 2) {
             tableDataTemp[i].status = '已完成';
-        } else if(tableDataTemp[i].status === 2) {
+        } else if (tableDataTemp[i].status === 1) {
             tableDataTemp[i].status = '进行中';
         }
     }
@@ -81,13 +82,46 @@ const checkDeleteTableRow = (rowIndex, rowId) => {
         })
 }
 
-// 初始化
+// 添加表格数据的表单
+const dialogInsertDataFormVisible = ref(false);
+
+const insertDataForm = reactive({
+    name: '',
+    date: ''
+});
+
+// 添加表格数据
+const insertData = () => {
+    axios.post('http://127.0.0.1:8080/testData/insert', {
+        name: insertDataForm.name,
+        date: insertDataForm.date,
+        status: 1
+    })
+        .then(function (response) {
+            // TEST 控制台输出提示
+            console.log('add ' + response.data + ' row');
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            // TODO 刷新表格时会重复访问数据库，注意性能问题
+            getTableData();
+        });
+}
+
+// TODO 添加表格数据确认
+const checkInsertData = () => {
+
+}
+
+// 页面初始化操作
 getTableData();
 </script>
 
 <template>
     <div class="table-functions">
-        <el-button type="primary" :icon="Plus" circle></el-button>
+        <el-button type="primary" :icon="Plus" circle @click="dialogInsertDataFormVisible = true"></el-button>
         <el-button type="success" :icon="Refresh" circle @click="getTableData"></el-button>
     </div>
     <div class="table-container">
@@ -106,11 +140,7 @@ getTableData();
             </el-table-column>
             <el-table-column label="操作" prop align="center">
                 <template #default="scope">
-                    <el-button
-                        type="primary"
-                        size="small"
-                        @click=""
-                    >编辑</el-button>
+                    <el-button type="primary" size="small">编辑</el-button>
                     <el-button
                         type="danger"
                         size="small"
@@ -123,6 +153,29 @@ getTableData();
     <!-- <div class="table-pagination">
         <el-button>分页预留</el-button>
     </div>-->
+
+    <el-dialog v-model="dialogInsertDataFormVisible" title="添加数据" width="25%">
+        <el-form class="dialog-insert-data-form" :model="insertDataForm">
+            <el-form-item label="名称">
+                <el-input v-model="insertDataForm.name" />
+            </el-form-item>
+            <el-form-item label="日期">
+                <el-date-picker
+                    class="test"
+                    v-model="insertDataForm.date"
+                    type="datetime"
+                    value-format="YYYY-MM-DD HH:mm:ss"
+                    style="width: 100%"
+                ></el-date-picker>
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <span class="dialog-insert-data-footer">
+                <el-button @click="dialogInsertDataFormVisible = false">取消</el-button>
+                <el-button type="primary" @click="insertData">确认</el-button>
+            </span>
+        </template>
+    </el-dialog>
 </template>
 
 <style scoped>
@@ -134,5 +187,9 @@ getTableData();
 .table-container {
     width: 80%;
     margin: 10px auto;
+}
+
+.test {
+    width: 300px;
 }
 </style>
